28fac401c33967c4dee1aa0a9b109a27a605d112
Matt Rosanio <mmrosani@syr.edu>
2016-04-08 09:56:25 -0700

    Add working login functionality

--- app/components/login-wrapper.js
+++ app/components/login-wrapper.js
@@ -4,6 +4,13 @@ export default Ember.Component.extend({
   actions: {
     signUp(params) {
       this.sendAction('signUp', params);
+    },
+    signIn() {
+      var params = {
+        username: this.get('username'),
+        password: this.get('password')
+      };
+      this.sendAction('signIn', params);
     }
   }
 });
--- app/routes/login.js
+++ app/routes/login.js
@@ -10,12 +10,30 @@ export default Ember.Route.extend({
   },
   actions: {
     signUp(params) {
-      console.log(params);
       var newUser = this.store.createRecord('user', params);
-      console.log(newUser);
       newUser.save();
       this.currentModel.currentUser.signInCurrentUser(newUser);
       this.transitionTo('index');
+    },
+    signIn(params) {
+      var foundUser = false;
+      var route = this;
+      route.currentModel.allUsers.forEach(function(user) {
+        console.log(user);
+        if(params.username === user.get('username')) {
+          if(params.password === user.get('password')) {
+            route.currentModel.currentUser.signInCurrentUser(user);
+            route.transitionTo('index');
+            foundUser = true;
+          } else {
+            alert('Incorrect Password');
+            foundUser = true;
+          }
+        }
+      });
+      if(!foundUser) {
+        alert('No user by that username was found');
+      }
     }
   }
 });
